d8de03a8c62aed8f2cac041a0bbaec7f799fff44
Qasim Fida <qasimfida0054@gmail.com>
2025-06-19 16:32:55 +0500

    fixed scroll issue

--- src/components/how-it-works.tsx
+++ src/components/how-it-works.tsx
@@ -5,24 +5,26 @@ import Thumbnail from "@/assets/thumbnails/How It Works.png"
 
 const HowItWorks = () => {
   return (
-    <Container size="md" id="how-it-works" className="py-16" >
+    <section id="how-it-works" className="pt-26 -mt-26"   >
+      <Container size="md" className="py-16" >
         <Title>
-            How It Works
+          How It Works
         </Title>
-        <div className="relative mt-4 overflow-hidden border rounded-lg bg-primary/5 border-primary/20">
-            <div className="aspect-video">
-              <video 
-                className="object-cover w-full h-full"
-                controls
-                src={VideoThumbnail}
-                poster={Thumbnail}
-              >
-                <source src="/assets/soldrip-explainer.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
+        <div className="overflow-hidden relative mt-4 rounded-lg border bg-primary/5 border-primary/20">
+          <div className="aspect-video">
+            <video
+              className="object-cover w-full h-full"
+              controls
+              src={VideoThumbnail}
+              poster={Thumbnail}
+            >
+              <source src="/assets/soldrip-explainer.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           </div>
-    </Container>
+        </div>
+      </Container>
+    </section>
   )
 }
 
